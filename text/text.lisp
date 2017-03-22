0df288b550a5e83d2286b9da29aae7774eafde62
soos-peter-levente <soos.peter.levente@protonmail.com>
2017-03-22 17:52:16 +0100

    remove the lambda and the conditional, too.

--- text/text.lisp
+++ text/text.lisp
@@ -5,9 +5,5 @@
 (defun get-text (id)
   (second (assoc id *string-set*)))
 
-(defun txt (string-symbol-id &optional &rest varlist)
-  (if (not varlist)
-      (format t (get-text string-symbol-id))
-      (apply #'format t (get-text string-symbol-id)
-             (mapcar #'(lambda (expr) expr)
-                     varlist))))
+(defun txt (string-symbol-id &optional &rest expr-list)
+  (apply #'format t (get-text string-symbol-id) expr-list))
